22e575f48f0fba339cdf619aa22114e30e049ee4
Tlangelani Mboweni <mbowenit@gmail.com>
2021-07-17 15:21:48 +0200

    add sub header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header';
+import SubHeader from './SubHeader';
 import '../sass/style.scss';
 
 const App = () => {
     return (
         <div className='app'>
             <Header />
+            <SubHeader />
         </div>
     )
 }
---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,18 @@
+const SubHeader = () => {
+    return (
+        <section className='sub-header'>
+            <img src='/img/logos.png' alt='Logos' />
+            <div>
+                <h4>Bundle with any Hulu plan & save</h4>
+                <h3>Get Hulu, Disney+, and ESPN+.</h3>
+                <a href='#' className='sub-link'>Details</a>
+            </div>
+            <div>
+                <a href='#' className='btn btn-outline'>Get Bundle</a>
+                <a href='#' className='sub-link'>Terms apply</a>
+            </div>
+        </section>
+    )
+}
+
+export default SubHeader
