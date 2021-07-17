3c33ae2503cbc976fe84727d896e581486cb1c2e
Tlangelani Mboweni <mbowenit@gmail.com>
2021-07-17 15:04:55 +0200

    update heading and add mobile styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 <div className='header-one'>Watch thousands of TV shows and movies.</div>
                 <div className='header-two'>HBO Max™, SHOWTIME®, CINEMAX® and STARZ® available as add-ons.</div>
                 <button className='btn btn-cta'>Start Your Free Trial</button>
-                <div className='legal'>Free trial for new & eligible returning subscribers only.</div>
+                <div className='legal-text'>Free trial for new & eligible returning subscribers only.</div>
             </div>
         </header>
     )
